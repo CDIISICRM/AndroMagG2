87e52cd7f8325c435653b050d85461b63ef4e2d1
BaptisteDerouin <baptiste.derouin@free.fr>
2014-03-13 11:15:49 +0100

    debut bdd

--- AndroMagG2/bin/classes.dex.d
+++ AndroMagG2/bin/classes.dex.d
@@ -1,7 +1,9 @@
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes.dex : \
+C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\dbAccess\DBHelper.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\AndroMag.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\BuildConfig.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\controller\AndroMag.class \
+C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\controller\NewActivity.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\R$attr.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\R$drawable.class \
 C:\Users\crm\Documents\MEGAsync\Baptiste\netBeans\AndroMagG2\AndroMagG2\bin\classes\fr\cdig2\androMag\R$layout.class \
--- AndroMagG2/src/dbAccess/DBHelper.java
+++ AndroMagG2/src/dbAccess/DBHelper.java
@@ -16,21 +16,58 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBHelper extends SQLiteOpenHelper{
     private static final String TAG = "DBHelper";
-    private static final String DATEBASE_NAME = "dbAndromag";
-    private static final int DATEBASE_VERSION = 1;
+    private static final String DATABASE_NAME = "dbAndromag";
+    private static final int DATABASE_VERSION = 1;
+    
+    public static final String DATABASE_TABLE_MAGAZINES = "magazines";
+    public static final String DATABASE_TABLE_CONTENUS  = "contenus";
+    public static final String DATABASE_TABLE_COMMENTAIRES = "commentaires";
+    public static final String DATABASE_TABLE_CONTIENT = "contient";
+    public static final String DATABASE_TABLE_COMMENTE = "commente";
+    public static final String DATABASE_TABLE_SCOPES = "scopes";
+    
+    //table magazines
+    public static final String DATABASE_CREATE_MAGAZINES = "CREATE TABLE " + DATABASE_TABLE_MAGAZINES + " (" +
+            "id INTEGER PRIMARY KEY, " + 
+            "nom VARCHAR(45) NOT NULL, " +
+            "prix INTEGER NOT NULL);";
+    
+    //table commentaires
+    public static final String DATABASE_CREATE_COMMENTAIRES = "CREATE TABLE " + DATABASE_TABLE_COMMENTAIRES + " (" + 
+            "id INTEGER PRIMARY KEY, " + 
+            "idScope INTEGER, " + 
+            "page INTEGER, " + 
+            "rate INTEGRER, " + 
+            "texte TEXT, " + 
+            "FOREIGN KEY(idScope) REFERENCES (scopes(id));";
+    
+    //table contenus
+     public static final String DATABASE_CREATE_CONTENUS = "CREATE TABLE " + DATABASE_TABLE_CONTENUS + " (" + 
+             "id INTEGER PRIMARY KEY, " +
+             "nom VARCHAR(45)); ";
+    
+     public static final String DATABASE_CREATE_SCOPES = "CREATE TABLE " + DATABASE_TABLE_SCOPES + " (" +
+             "id INTEGER PRIMARY KEY, " + 
+             "nom VARCHAR(45));";
+     
+     public static final String DATABASE_CREATE_CONTIENT = "CREATE TABLE " + DATABASE_TABLE_CONTIENT + " (" + 
+             "idContenu INTEGER PRIMARY KEY, " + 
+             "idMagazine INTEGER PRIMARY KEY, " + 
+             "FOREIGN KEY ";
+    
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
