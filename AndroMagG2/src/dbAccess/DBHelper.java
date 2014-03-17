ebec4b956e77bc65eb18dd7118c909eb593adc69
BaptisteDerouin <baptiste.derouin@free.fr>
2014-03-17 15:52:16 +0100

    tous les CREATE TABLE

--- AndroMagG2/src/dbAccess/DBHelper.java
+++ AndroMagG2/src/dbAccess/DBHelper.java
@@ -22,15 +22,22 @@ public class DBHelper extends SQLiteOpenHelper{
     public static final String DATABASE_TABLE_MAGAZINES = "magazines";
     public static final String DATABASE_TABLE_CONTENUS  = "contenus";
     public static final String DATABASE_TABLE_COMMENTAIRES = "commentaires";
-    public static final String DATABASE_TABLE_CONTIENT = "contient";
-    public static final String DATABASE_TABLE_COMMENTE = "commente";
+    public static final String DATABASE_TABLE_COMMENTAIRE_MAGAZINE = "commentaire_magazine";
+    public static final String DATABASE_TABLE_COMMENTAIRE_RUBRIQUE = "commentaire_rubrique";
+    public static final String DATABASE_TABLE_COMMENTAIRE_ARTICLE = "commentaire_article";
     public static final String DATABASE_TABLE_SCOPES = "scopes";
+    public static final String DATABASE_TABLE_RUBRIQUES = "rubriques";
+    public static final String DATABASE_TABLE_NUMEROS = "numeros";
+    public static final String DATABASE_TABLE_ARTICLES = "articles";
+    
     
     //table magazines
     public static final String DATABASE_CREATE_MAGAZINES = "CREATE TABLE " + DATABASE_TABLE_MAGAZINES + " (" +
             "id INTEGER PRIMARY KEY, " + 
             "nom VARCHAR(45) NOT NULL, " +
-            "prix INTEGER NOT NULL);";
+            "idContenu INTEGER NOT NULL, " +
+            "prix INTEGER NOT NULL, "
+            + "FOREIGN KEY(idContenu) REFERENCES(" + DATABASE_TABLE_CONTENUS + "(id));";
     
     //table commentaires
     public static final String DATABASE_CREATE_COMMENTAIRES = "CREATE TABLE " + DATABASE_TABLE_COMMENTAIRES + " (" + 
@@ -39,7 +46,7 @@ public class DBHelper extends SQLiteOpenHelper{
             "page INTEGER, " + 
             "rate INTEGRER, " + 
             "texte TEXT, " + 
-            "FOREIGN KEY(idScope) REFERENCES (scopes(id));";
+            "FOREIGN KEY(idScope) REFERENCES("+DATABASE_TABLE_SCOPES+"(id));";
     
     //table contenus
      public static final String DATABASE_CREATE_CONTENUS = "CREATE TABLE " + DATABASE_TABLE_CONTENUS + " (" + 
@@ -50,12 +57,41 @@ public class DBHelper extends SQLiteOpenHelper{
              "id INTEGER PRIMARY KEY, " + 
              "nom VARCHAR(45));";
      
-     public static final String DATABASE_CREATE_CONTIENT = "CREATE TABLE " + DATABASE_TABLE_CONTIENT + " (" + 
-             "idContenu INTEGER PRIMARY KEY, " + 
-             "idMagazine INTEGER PRIMARY KEY, " + 
-             "FOREIGN KEY ";
+     public static final String DATABASE_CREATE_COMMENTAIRE_MAGAZINE = "CREATE TABLE " + DATABASE_TABLE_COMMENTAIRE_MAGAZINE + " (" + 
+             "idCommentaire INTEGER PRIMARY KEY, " + 
+             "idMagazine INTEGER PRIMARY KEY, " +
+             "FOREIGN KEY(idCommentaire) REFERENCES(" + DATABASE_TABLE_COMMENTAIRES + "(id), "
+             + "FOREIGN KEY(idMagazine) REFERENCES(" + DATABASE_TABLE_MAGAZINES + "(id));";
+     
+    public static final String DATABASE_CREATE_COMMENTAIRE_ARTICLE = "CREATE TABLE " + DATABASE_TABLE_COMMENTAIRE_ARTICLE + " (" + 
+             "idCommentaire INTEGER PRIMARY KEY, " + 
+             "idArticle INTEGER PRIMARY KEY, " +
+             "FOREIGN KEY(idCommentaire) REFERENCES(" + DATABASE_TABLE_COMMENTAIRES + "(id), "
+             + "FOREIGN KEY(idArticle) REFERENCES(" + DATABASE_TABLE_ARTICLES + "(id));";
+           
+    public static final String DATABASE_CREATE_ARTICLE = "CREATE TABLE " + DATABASE_TABLE_ARTICLES + " ("
+            + "id INTEGER PRIMARY KEY, "
+            + "titre VARCHAR(45) NOT NULL, "
+            + "idNo INTEGER NOT NULL, "
+            + "FOREING KEY(idNo) REFERENCES(" + DATABASE_TABLE_NUMEROS + "(id));";
     
+    public static final String DATABASE_CREATE_NUMEROS = "CREATE TABLE " + DATABASE_TABLE_NUMEROS + " ("
+            + "id INTEGER PRIMARY KEY, "
+            + "no INTEGER NOT NULL, "
+            + "idMag INTEGER NOT NULL, "
+            + "FOREIGN KEY(idMag) REFERENCES(" + DATABASE_TABLE_MAGAZINES + "(id));";
+    
+    public static final String DATABASE_CREATE_RUBRIQUES = "CREATE TABLE " + DATABASE_TABLE_RUBRIQUES + " ("
+            + "id INTEGER PRIMARY KEY, "
+            + "nom VARCHAR(45) NOT NULL);";
 
+    public static final String DATABASE_CREATE_COMMENTAIRE_RUBRIQUE = "CREATE TABLE " + DATABASE_TABLE_COMMENTAIRE_RUBRIQUE + " ("
+            + "idRubrique INTEGER PRIMARY KEY, "
+            + "idCommentaire INTEGER PRIMARY KEY, "
+            + "FOREIGN KEY(idRubrique) REFERENCES(" + DATABASE_TABLE_RUBRIQUES + "(id), "
+            + "FOREIGN KEY(idCommentaire) REFERENCES(" + DATABASE_TABLE_COMMENTAIRES + "(id));";
+    
+    
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
